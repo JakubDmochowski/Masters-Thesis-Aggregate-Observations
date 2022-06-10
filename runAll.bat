f308f897339b1ee4d57371959e9e8b0f24ae3f33
Jakub Dmochowski <jakub@dmochowski.co>
2022-06-10 20:06:43 +0200

    env start in project folder

--- runAll.bat
+++ runAll.bat
@@ -1,3 +1,3 @@
 @echo off
 
-@powershell -ExecutionPolicy bypass -noexit "venv\Scripts\Activate.ps1"
+@powershell -ExecutionPolicy bypass -noexit "venv\Scripts\Activate.ps1; cd aggregateTrainingTest"
